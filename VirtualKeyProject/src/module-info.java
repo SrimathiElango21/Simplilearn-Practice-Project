eb8530ca92a319e263fc1b778bc49710c7b3ecd5
Srimathi <srimathielangoppm@gmail.com>
2022-07-29 14:44:55 +0530

    FirstCommit

--- VirtualKeyProject/src/module-info.java
+++ VirtualKeyProject/src/module-info.java
@@ -1,6 +1,2 @@
-module Directory {
-	exports org.example.virtualkey.services;
-	exports org.example.virtualkey;
-	exports org.example.virtualkey.entities;
-	exports org.example.virtualkey.screens;
+module VirtualKeyProject {
 }
---
+++ VirtualKeyProject/src/virtualkey/VirtualKeyApplication.java
@@ -0,0 +1,11 @@
+package virtualkey;
+
+public class VirtualKeyApplication {
+
+	public static void main(String[] args) {
+		WelcomeScreen welcome = new WelcomeScreen();
+    	welcome.introWS();
+    	welcome.GetUserInput();
+	}
+
+}
