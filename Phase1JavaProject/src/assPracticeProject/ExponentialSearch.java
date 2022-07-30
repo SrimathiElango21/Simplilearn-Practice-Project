a7372b12a4d6e4eb418979eff4aa2d4766fb3bd2
Srimathi <srimathielangoppm@gmail.com>
2022-07-30 16:38:18 +0530

    Fourteenth Commit

---
+++ Phase1JavaProject/src/assPracticeProject/ExponentialSearch.java
@@ -0,0 +1,25 @@
+package assPracticeProject;
+
+import java.util.Arrays;
+
+public class ExponentialSearch {
+
+	static int exponentialSearch(int arr[],int n, int x)
+	{
+		if (arr[0] == x)
+			return 0;
+		
+		int i = 1;
+		while (i < n && arr[i] <= x)
+			i = i*2;
+		return Arrays.binarySearch(arr, i/2, 
+         Math.min(i, n-1), x);
+}
+	public static void main(String args[])
+	{
+		int arr[] = {10, 20, 30, 40, 50};
+		int x = 30;
+		int result = exponentialSearch(arr, arr.length, x);
+		System.out.println((result < 0) ? "Element is not present in array" :"Element is present at index " + result);
+		}
+}
